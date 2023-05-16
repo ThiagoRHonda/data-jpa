be9b76b6415d9e01ec0dae3340a050554f5c56b7
ThiagoRHonda <trossettohonda@gmail.com>
2023-05-16 20:18:47 -0300

    Alterações para customer

--- src/main/java/com/aula/datajpa/Repository/CustomerRepository.java
+++ src/main/java/com/aula/datajpa/Repository/CustomerRepository.java
@@ -3,12 +3,19 @@ package com.aula.datajpa.Repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.aula.datajpa.Model.Customer;
 
+@RepositoryRestResource(collectionResourceRel = "customer", path = "customer")
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     
-    List<Customer> findByLastName(String lastName);
+    //List<Customer> findByLastName(String lastName);
 
     Customer findById(long id);
+
+    List<Customer> findByLastName(@Param("customer") String name);
+
+    List<Customer> findByFirstName(@Param("customer") String name);
 }
